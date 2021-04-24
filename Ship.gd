e955441b1c7a3d6c3ee3103d2c18d3c93da8772a
Mark Steve Samson <hello@marksteve.com>
2021-04-24 21:47:23 +0800

    Add ship model

--- Ship.gd
+++ Ship.gd
@@ -5,12 +5,16 @@ signal destroyed
 const BOOST = 1.0
 onready var top_rocket = $TopRocket
 onready var top_rocket_particles = $TopRocket/Particles
+onready var top_rocket_light = $TopRocket/SpotLight
 onready var bottom_rocket = $BottomRocket
 onready var bottom_rocket_particles = $BottomRocket/Particles
+onready var bottom_rocket_light = $BottomRocket/SpotLight
 onready var right_rocket = $RightRocket
 onready var right_rocket_particles = $RightRocket/Particles
+onready var right_rocket_light = $RightRocket/SpotLight
 onready var left_rocket = $LeftRocket
 onready var left_rocket_particles = $LeftRocket/Particles
+onready var left_rocket_light = $LeftRocket/SpotLight
 var is_destroyed = false
 
 func apply_rotation(position: Vector3):
@@ -22,18 +26,27 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("ui_up"):
 		apply_impulse(apply_rotation(top_rocket.translation), apply_rotation(Vector3(0, -BOOST, 0)))
+		top_rocket_light.light_energy = lerp(top_rocket_light.light_energy, 16, 0.5)
 	elif Input.is_action_pressed("ui_down"):
 		apply_impulse(apply_rotation(bottom_rocket.translation), apply_rotation(Vector3(0, BOOST, 0)))
+		bottom_rocket_light.light_energy = lerp(bottom_rocket_light.light_energy, 16, 0.5)
 		
 	if Input.is_action_pressed("ui_right"):
 		apply_impulse(apply_rotation(right_rocket.translation), apply_rotation(Vector3(-BOOST, 0, 0)))
+		right_rocket_light.light_energy = lerp(right_rocket_light.light_energy, 16, 0.5)
 	elif Input.is_action_pressed("ui_left"):
 		apply_impulse(apply_rotation(left_rocket.translation), apply_rotation(Vector3(BOOST, 0, 0)))
+		left_rocket_light.light_energy = lerp(left_rocket_light.light_energy, 16, 0.5)
 
 	top_rocket_particles.emitting = Input.is_action_pressed("ui_up")
 	bottom_rocket_particles.emitting = Input.is_action_pressed("ui_down")
 	right_rocket_particles.emitting = Input.is_action_pressed("ui_right")
 	left_rocket_particles.emitting = Input.is_action_pressed("ui_left")
+	
+	top_rocket_light.light_energy = lerp(top_rocket_light.light_energy, 0, 0.5)
+	bottom_rocket_light.light_energy = lerp(bottom_rocket_light.light_energy, 0, 0.5)
+	right_rocket_light.light_energy = lerp(right_rocket_light.light_energy, 0, 0.5)
+	left_rocket_light.light_energy = lerp(left_rocket_light.light_energy, 0, 0.5)
 
 func _on_Ship_body_entered(body):
 	if is_destroyed:
