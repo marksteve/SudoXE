c371178a41a5742b8fb1ae9b81be1327517b20d7
Mark Steve Samson <hello@marksteve.com>
2021-04-24 15:24:50 +0800

    Add sky lighting

--- Camera.gd
+++ Camera.gd
@@ -1,4 +1,6 @@
 extends Camera
 
+onready var ship = $"../Ship"
+
 func _process(delta):
-	self.translation.y = lerp(self.translation.y, $"../Ship".translation.y, 0.8)
+	self.translation.y = lerp(self.translation.y, ship.translation.y, 0.8)
