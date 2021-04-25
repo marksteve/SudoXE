751c089e56f5e81544e99f44ae007e20f8279b3f
Mark Steve Samson <hello@marksteve.com>
2021-04-25 15:50:05 +0800

    Clean up screen code

--- UI.gd
+++ UI.gd
@@ -11,8 +11,6 @@ var screen = ""
 func change_screen(new_screen: String):
 	screen = new_screen
 	emit_signal("screen_changed", new_screen)
-
-func update_screen():
 	get_tree().paused = screen == "title"
 	title_screen.visible = screen == "title"
 	game_over.visible = screen == "game_over"
@@ -22,7 +20,6 @@ func update_screen():
 func _ready():
 	randomize()
 	change_screen("title")
-	get_tree().paused = true
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
@@ -30,12 +27,10 @@ func _input(event):
 			change_screen("game")
 		elif screen == "game_over":
 			change_screen("title")
-		call_deferred("update_screen")
 
 func _on_Ship_depth_changed(depth, velocity):
 	hud_depth.text = "%.3f m" % depth
 	hud_velocity.text = "%3.f m/s" % velocity
 
 func _on_Ship_destroyed():
-	game_over.visible = true
 	change_screen("game_over")
