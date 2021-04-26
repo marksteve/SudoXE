801db0cb2c524824630d16a128aaa219a0465246
Mark Steve Samson <hello@marksteve.com>
2021-04-26 10:21:14 +0800

    Polish and icons

--- models/City.gd
+++ models/City.gd
@@ -7,6 +7,7 @@ onready var sfx_explode = $SFXExplode
 func _on_Ship_depth_changed(depth, velocity):
 	var scale = min((20 + depth) / 20, 2.0)
 	explosion.scale = Vector3(scale, scale, scale)
+	explosion.rotation.y = PI * scale / 2
 	explosion_light.omni_range = scale * 100
 
 
